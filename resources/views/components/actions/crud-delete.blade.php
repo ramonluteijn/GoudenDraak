df8c10a3f49e42050153ed0fe64dca127b0461ab
ramon luteijn <ramon@intractief.nl>
2025-06-21 13:18:32 +0200

    feat: enhance hero section styling with updated font weights and sizes removed unused components

--- resources/views/components/actions/crud-delete.blade.php
+++
@@ -1,7 +0,0 @@
-@props(['item', 'route', 'title', 'message'])
-<form method="POST" action="{{ route($route, ['id' => $item->id]) }}" class="mt-4">
-    @method('DELETE')
-    @csrf
-    <button id="openModalDeleteButton" data-modal-id="deleteModal" type="button" class="button delete">{{ $title }}</button>
-    <x-modal id="deleteModal" title="{{ $title }}" message="{{ $message }}" />
-</form>
--- resources/views/components/forms/input-switch.blade.php
+++
@@ -1,6 +0,0 @@
-@props(["name","checked" => false,"disabled" => false, "label" => ''])
-
-<div class="form-group flex items-center my-2">
-    <input type="checkbox" name="{{$name}}" id="{{$label}}" @if($checked) checked @endif @if($disabled) disabled @endif class="form-checkbox h-5 w-5 text-blue-600">
-    <label for="{{$label}}" class="ms-3">{{ ucfirst($label) }}</label>
-</div>
